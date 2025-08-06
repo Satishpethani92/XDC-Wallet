2a64881ae1cc61969fd026899eba9bb029bf1a5c
satishpethani <satyapethani@gmail.com>
2025-08-06 12:04:50 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const offlineConfig = require('./builds/offlineConfig');
 const liveConfig = require('./builds/liveConfig');
+const path = require('path');
 
 let baseConfig = {};
 if (process.env.BUILD === 'offline') {
@@ -8,17 +9,28 @@ if (process.env.BUILD === 'offline') {
   baseConfig = liveConfig;
 }
 
-// Merge your base configuration with the transpileDependencies option
-const finalConfig = {
-  ...baseConfig, // This includes all settings from your live/offline config
+module.exports = {
+  ...baseConfig,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@ledgerhq/cryptoassets/data/eip712':
+          '@ledgerhq/cryptoassets/lib/data/eip712',
+        '@ledgerhq/cryptoassets/data/evm/index':
+          '@ledgerhq/cryptoassets/lib/data/evm/index',
+        '@ledgerhq/devices/ble/receiveAPDU':
+          '@ledgerhq/devices/lib/ble/receiveAPDU',
+        '@ledgerhq/devices/ble/sendAPDU': '@ledgerhq/devices/lib/ble/sendAPDU',
+        '@ledgerhq/devices/hid-framing': '@ledgerhq/devices/lib/hid-framing',
+        '@ledgerhq/domain-service/signers/index':
+          '@ledgerhq/domain-service/lib/signers/index'
+      }
+    }
+  },
   transpileDependencies: [
     'web-vitals',
     '@amplitude/plugin-web-vitals-browser',
     '@ensdomains/address-encoder',
-    '@enkryptcom/name-resolution',
-    '@ledgerhq/cryptoassets/data/eip712',
-    '@ledgerhq/cryptoassets/data/evm/index'
+    '@enkryptcom/name-resolution'
   ]
 };
-
-module.exports = finalConfig;
