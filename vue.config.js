61ce18cdfbf6e2d742d646859cfe10352bf3f1c8
satishpethani <satyapethani@gmail.com>
2025-08-06 12:10:55 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const offlineConfig = require('./builds/offlineConfig');
 const liveConfig = require('./builds/liveConfig');
-const path = require('path');
 
 let baseConfig = {};
 if (process.env.BUILD === 'offline') {
@@ -9,24 +8,9 @@ if (process.env.BUILD === 'offline') {
   baseConfig = liveConfig;
 }
 
-module.exports = {
-  ...baseConfig,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@ledgerhq/cryptoassets/data/eip712':
-          '@ledgerhq/cryptoassets/lib/data/eip712',
-        '@ledgerhq/cryptoassets/data/evm/index':
-          '@ledgerhq/cryptoassets/lib/data/evm/index',
-        '@ledgerhq/devices/ble/receiveAPDU':
-          '@ledgerhq/devices/lib/ble/receiveAPDU',
-        '@ledgerhq/devices/ble/sendAPDU': '@ledgerhq/devices/lib/ble/sendAPDU',
-        '@ledgerhq/devices/hid-framing': '@ledgerhq/devices/lib/hid-framing',
-        '@ledgerhq/domain-service/signers/index':
-          '@ledgerhq/domain-service/lib/signers/index'
-      }
-    }
-  },
+// Merge your base configuration with the transpileDependencies option
+const finalConfig = {
+  ...baseConfig, // This includes all settings from your live/offline config
   transpileDependencies: [
     'web-vitals',
     '@amplitude/plugin-web-vitals-browser',
@@ -34,3 +18,5 @@ module.exports = {
     '@enkryptcom/name-resolution'
   ]
 };
+
+module.exports = finalConfig;
