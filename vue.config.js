aaa3592a3c2542a53e77898ca156dde6074486c4
satishpethani <satyapethani@gmail.com>
2026-02-10 10:56:00 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -27,8 +27,7 @@ const finalConfig = {
     resolve: {
       alias: {
         // This forces Webpack to use the standard JS file, bypassing the broken .mjs file
-        'libsodium-wrappers': path.resolve(__dirname, 'node_modules/libsodium-wrappers/dist/modules/libsodium-wrappers.js'),
-        'libsodium': path.resolve(__dirname, 'node_modules/libsodium-wrappers/dist/modules/libsodium.js'),
+        'libsodium-wrappers': path.resolve(__dirname, 'node_modules/libsodium-wrappers/dist/modules/libsodium-wrappers.js')
       }
     }
   }
